c46b9d8113b8afd922fc30c5b3ff53a620644069
Emiliano Gonzalez <93420074+EmGoAR@users.noreply.github.com>
2023-01-17 00:32:03 -0300

    sidebar finished

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <h1> lets create discord app!</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chat */}
+    </div>
+  );
+}
+
+export default App;
